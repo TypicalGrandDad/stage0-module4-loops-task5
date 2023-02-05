11831a277283dac6b8bd505689e827dab306a23b
Nikolay <124496633+TypicalGrandDad@users.noreply.github.com>
2023-02-05 20:46:22 +0300

    Update Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,96 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        if(cathetusLength==3){
+            for(int x =0; x<cathetusLength;x++){
+                for(int y=0; y<cathetusLength;y++){
+                    if(x==0){
+                        if(y==0){
+                            System.out.println("8");
+                        }
+                    }
+                    if(x==1){
+                        if(y==0){
+                            System.out.print("8");
+                        }
+                        if(y==1){
+                            System.out.println("8");
+                        }
+                    }
+                    if(x==2){
+                        if(y==0){
+                            System.out.print("8");
+                        }
+                        if(y==1){
+                            System.out.print("8");
+                        }
+                        if(y==2){
+                            System.out.print("8");
+                        }
+                    }
+                }
+            }
+        }
+        else{
+            for(int x =0; x<cathetusLength;x++){
+                for(int y=0; y<cathetusLength;y++){
+                    if(x==0){
+                        if(y==0){
+                            System.out.println("8");
+                        }
+                    }
+                    if(x==1){
+                        if(y==0){
+                            System.out.print("8");
+                        }
+                        if(y==1){
+                            System.out.println("8");
+                        }
+                    }
+                    if(x==2){
+                        if(y==0){
+                            System.out.print("8");
+                        }
+                        if(y==1){
+                            System.out.print("8");
+                        }
+                        if(y==2){
+                            System.out.println("8");
+                        }
+                    }
+                    if(x==3){
+                        if(y==0){
+                            System.out.print("8");
+                        }
+                        if(y==1){
+                            System.out.print("8");
+                        }
+                        if(y==2){
+                            System.out.print("8");
+                        }
+                        if(y==3){
+                            System.out.println("8");
+                        }
+                    }
+                    if(x==4){
+                        if(y==0){
+                            System.out.print("8");
+                        }
+                        if(y==1){
+                            System.out.print("8");
+                        }
+                        if(y==2){
+                            System.out.print("8");
+                        }
+                        if(y==3){
+                            System.out.print("8");
+                        }
+                        if(y==4){
+                            System.out.print("8");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
