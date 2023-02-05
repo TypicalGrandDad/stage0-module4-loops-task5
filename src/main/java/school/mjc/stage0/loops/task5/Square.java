927edb20407027b28e12e08dc20fd7502826577a
Nikolay <124496633+TypicalGrandDad@users.noreply.github.com>
2023-02-05 21:16:25 +0300

    Update Square.java

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,15 +2,24 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-        for(int x = 0; x<sideLength; x++){
+          for(int x = 0; x<sideLength; x++){
             for(int y =0; y<sideLength;y++){
                 if(x==0 || x==sideLength-1){
-                    System.out.print("8");
-                    if(y==sideLength-1 && x==0){
-                        System.out.println();
+                    if(x==0){
+                        if(y==sideLength-1){
+                        System.out.println("8");}
+                        else{
+                            System.out.print("8");
+                        }
                     }
+                    if(x==sideLength-1){
+                        if(y==sideLength-1){
+                            System.out.println("8");
+                        }
+                        else {
+                        System.out.print(8);
+                    }}
                 }
-
                 else{
                     if(y==1 || y==sideLength){
                         System.out.print("8");
